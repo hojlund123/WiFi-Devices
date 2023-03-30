48055ba1bfbcc100c5202d3d6174ad64924b6f61
hojlund123 <zere0.mail@gmail.com>
2023-03-30 22:28:35 +0200

    Testing diff modules

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from scapy.all import ARP, Ether, srp
-from mac_vendor_lookup import MacLookup
 import socket
+from mac_vendor_lookup import MacLookup
+from pcapfile import savefile
 
 app = Flask(__name__)
 
@@ -14,26 +15,69 @@ def index():
     arp_request = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=network)
 
     # Send the ARP request and wait for the response
-    responses, _ = srp(arp_request, timeout=1, verbose=True)
+    responses, _ = srp(arp_request, timeout=1, verbose=False)
 
-    # Parse the responses and extract the device information
+    # Update local file for MacLookup vendors
+    MacLookup().update_vendors()
+
+    # Parse the responses and extract the MAC addresses and IP addresses
     devices = []
     for response in responses:
-        ip_address = response[1].psrc
-        hostname = socket.getfqdn(ip_address)
         mac_address = response[1].hwsrc
-        vendor_info = MacLookup().lookup(mac_address)
-        os_info = get_os_info(ip_address)
-        devices.append((ip_address, hostname, mac_address, vendor_info["name"], os_info))
+        ip_address = response[1].psrc
+        hostname = socket.getfqdn(ip_address) # Get the hostname using DNS
 
+        # Create a MacLookup object
+        mac_lookup = MacLookup()
+
+        # Look up the vendor information for a MAC address
+        vendor_info = mac_lookup.lookup(mac_address)
+
+        # OS Info
+        os_info = get_os_info(ip_address)
+
+        # Append all to devices
+        devices.append((ip_address, hostname, mac_address, vendor_info, os_info))
+        print(ip_address, hostname, mac_address, vendor_info, os_info)
     return render_template("index.html", devices=devices)
 
 def get_os_info(ip_address):
-    # TODO: Implement this function to obtain the OS information for a device
-    # You can use packet sniffing tools like Wireshark to capture network traffic
-    # and analyze it to obtain this information, or use other techniques like
-    # fingerprinting or active scanning.
-    return "Unknown"
+    # Define the network interface to capture packets on
+    iface = "wlan0"
+
+    # Define the number of packets to capture
+    num_packets = 100
+
+    # Define the filter expression to capture packets for the specified IP address
+    filter_expr = f"host {ip_address}"
+
+    # Capture the packets and save them to a temporary file
+    capture_file = f"/tmp/capture_{ip_address}.pcap"
+    os.system(f"tcpdump -i {iface} -c {num_packets} -w {capture_file} {filter_expr}")
+
+    # Read the captured packets from the file
+    with open(capture_file, "rb") as f:
+        packets = savefile.load_savefile(f).packets
+
+    # Extract the operating system information from the packets
+    os_info = set()
+    for packet in packets:
+        ether = Ether(packet.raw())
+        if "IP" in ether:
+            ip = ether["IP"]
+            if "TCP" in ip:
+                tcp = ip["TCP"]
+                os_info.add((tcp["window"], tcp["flags"], tcp["options"]))
+
+    # Convert the operating system information to a string representation
+    os_str = ""
+    for os_entry in os_info:
+        os_str += f"Window size: {os_entry[0]}, Flags: {os_entry[1]}, Options: {os_entry[2]}\n"
+
+    # Remove the temporary capture file
+    os.system(f"rm {capture_file}")
+
+    return os_str
 
 if __name__ == "__main__":
     app.run(debug=True)
